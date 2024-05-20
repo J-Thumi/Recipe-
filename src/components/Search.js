b41838e7f2dad5105c534d734a4a13fd4820014f
Josphat Waweru Thumi <thumijosphat47@gmail.com>
2024-05-20 21:19:45 +0300

    second

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,24 @@
 import React , {useEffect, useState} from 'react'
 
+
+const URL ="https://api.spoonacular.com/recipes/complexSearch"
+const API_KEY="48845d35318a4d67a1e1ecf4333e6d14"
+
+//
+
 function Search() {
 
-const [search,setSearch]=useState("haa")
+const [search,setSearch]=useState("pizza")
 
 useEffect(
-    ()=>{},[search]
+    ()=>{
+        async function fetchData(){
+const res = await fetch(`${URL}?query=${search}&apiKey=${API_KEY}`)
+const data = await res.json()
+console.log(data.results)
+        }
+        fetchData()
+    },[search]
 )
 
   return (
